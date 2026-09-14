9321a77ccf6546b3924ee1f9a7ea146eb773ddd9
Debashis Saha <debashissaha.bst@gmail.com>
2026-09-14 22:22:11 +0600

    Initial commit

---
+++ importlearn/script.js
@@ -0,0 +1,5 @@
+import {sum, mul, g, PI} from './math.js';
+import randomWord from 'random-word';
+
+console.log(sum(8, 4));
+console.log(randomWord());
---
+++ script.js
@@ -0,0 +1,16 @@
+let n = 5;
+
+for (let i = 0; i < n; i++){
+    console.log("hello, ", i);
+}
+
+console.log("bye!");
+
+
+const math = require("./math");
+
+console.log(math);
+
+console.log(math.sum(5, 3));
+
+console.log(math.mul(5, 4));
