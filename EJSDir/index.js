4b800cdffa9ec363847cb778e8497a3e0a633494
Debashis Saha <debashissaha.bst@gmail.com>
2026-09-16 23:59:40 +0600

    passing data ejs

--- EJSDir/index.js
+++ EJSDir/index.js
@@ -3,10 +3,18 @@ const app = express();
 
 const port = 8080;
 
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render("home.ejs");
+});
+
+
+app.get('/rolldice', (req, res) => {
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { num: diceValue });
 });
 
 app.listen(port, () => {
